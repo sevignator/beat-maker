00d2a72f2483f57c44bfa5e24a18869252eff6b0
Mikaël Sévigny <sevignator@hotmail.com>
2024-12-03 10:17:37 -0500

    Revert range function to JS

--- src/functions/range.js
+++ src/functions/range.js
@@ -2,8 +2,8 @@
  * @function Function based on the `range()` built-in function in Python.
  * Implementation written by Josh W. Comeau (https://github.com/joshwcomeau).
  */
-export default function range(start: number, end?: number, step = 1) {
-  const output: number[] = [];
+export default function range(start, end, step = 1) {
+  const output = [];
 
   if (start) {
     for (let i = start; i < end; i += step) {
