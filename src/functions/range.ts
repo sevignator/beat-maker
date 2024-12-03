932656849bdaa1f9588e90a577bdfb980fa117c5
Mikaël Sévigny <sevignator@hotmail.com>
2024-12-03 10:20:48 -0500

    Fix type issue with range function

--- src/functions/range.ts
+++ src/functions/range.ts
@@ -2,8 +2,12 @@
  * @function Function based on the `range()` built-in function in Python.
  * Implementation written by Josh W. Comeau (https://github.com/joshwcomeau).
  */
-export default function range(start, end, step = 1) {
-  const output = [];
+export default function range(start: number, end?: number, step = 1) {
+  const output: number[] = [];
+
+  if (end === undefined) {
+    end = start;
+  }
 
   if (start) {
     for (let i = start; i < end; i += step) {
