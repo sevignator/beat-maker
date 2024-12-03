752dcf5f5bec89dd504c1178e5f06c3694751f5e
Mikaël Sévigny <sevignator@hotmail.com>
2024-12-03 10:14:52 -0500

    Decrease header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Controls from './Controls';
 function Header() {
   return (
     <div className='bg-gray-100 border-b mb-12'>
-      <header className='flex justify-between items-center py-4 max-w-screen-md mx-auto'>
+      <header className='flex justify-between items-center py-3 max-w-screen-md mx-auto'>
         <h1 className='text-3xl tracking-tight font-bold'>Beat Maker</h1>
         <Controls />
       </header>
